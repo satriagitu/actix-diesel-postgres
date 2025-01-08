66f1f44e5a18a82037369a1575a37922d277c604
satria <satria.gitu4@gmail.com>
2025-01-08 15:53:25 +0700

    add web::block for task blocking

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,30 +1,46 @@
 // handlers.rs
-use actix_web::{get, post, web, HttpResponse, Responder};
+use actix_web::{error, get, post, web, HttpResponse, Responder};
 use crate::actions::{find_user_by_uid, insert_new_user};
 use crate::db::DbPool;
 use crate::models::NewUser;
 use uuid::Uuid;
 
 #[get("/users/{id}")]
-async fn get_users(pool: web::Data<DbPool>, user_id: web::Path<Uuid>) -> impl Responder {
-    let conn = &mut pool.get().expect("Failed to get DB connection");
+async fn get_users(pool: web::Data<DbPool>, user_id: web::Path<Uuid>) -> actix_web::Result<impl Responder> {
+    // let conn = &mut pool.get().expect("Failed to get DB connection");
+    let user_uid = user_id.into_inner();
+    let user = web::block(move || {
+        let mut conn = pool.get()?; // Dapatkan koneksi dari pool
+        find_user_by_uid(&mut conn, user_uid) // Masukkan data user ke database
+    })
+    .await?
+    .map_err(error::ErrorInternalServerError)?;
 
-    match find_user_by_uid(conn, user_id.into_inner()) {
-        Ok(Some(user)) => HttpResponse::Ok().json(user),
-        Ok(None) => HttpResponse::NotFound().body("User not found"),
-        Err(err) => HttpResponse::InternalServerError().body(err.to_string()),
-    }
+    Ok(match user {
+        // user was found; return 200 response with JSON formatted user object
+        Some(user) => HttpResponse::Ok().json(user),
+
+        // user was not found; return 404 response with error message
+        None => HttpResponse::NotFound().body(format!("No user found with UID: {user_uid}")),
+    })
 }
 
 #[post("/users")]
 async fn create_user(
     pool: web::Data<DbPool>,
     new_user: web::Json<NewUser>,
-) -> impl Responder {
-    let conn = &mut pool.get().expect("Failed to get DB connection");
+) -> actix_web::Result<impl Responder> {
+    let new_user = new_user.into_inner(); // Konversi Json ke struct NewUser
 
-    match insert_new_user(conn, &new_user.name, &new_user.email) {
-        Ok(user) => HttpResponse::Created().json(user),
-        Err(err) => HttpResponse::InternalServerError().body(err.to_string()),
-    }
+    // Memindahkan operasi ke thread pool untuk mencegah blocking
+    let user = web::block(move || {
+        let mut conn = pool.get()?; // Dapatkan koneksi dari pool
+        insert_new_user(&mut conn, &new_user.name, &new_user.email) // Masukkan data user ke database
+    })
+    .await?
+    // map diesel query errors to a 500 error response
+    .map_err(error::ErrorInternalServerError)?;
+
+    // user was added successfully; return 201 response with new user info
+    Ok(HttpResponse::Created().json(user))
 }
